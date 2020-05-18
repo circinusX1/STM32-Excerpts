aa6c98bb9c73a34aacae309c53a8237b742bfefb
C.O.Marius <marrius9876@gmail.com>
2020-05-18 11:39:10 -0400

    Create BME280.h

---
+++ BME280.h
@@ -0,0 +1,14 @@
+#ifndef __BME280_H__
+#define __BME280_H__
+
+#include "i2c.h"
+#include "s2w.h"
+#include "sensors.h"
+
+#define BME280_ADDRESS                (0x76) /*scanned*/
+
+int BME280_init(struct ThpSensors* ps);
+struct Thp* BME280_read(struct ThpSensors* ps);
+struct Thp*  BME280_Calibrate(struct ThpSensors* ps);
+
+#endif
